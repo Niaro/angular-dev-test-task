08336ad0d467d0f47747eeb3d4ae166d09b13576
Maxim Unguryan <makswww1@gmail.com>
2022-02-24 01:16:09 +0300

    added method for getting geo data about city

---
+++ libs/weather-forecast/services/src/lib/weather-forecast-api.interface.ts
@@ -0,0 +1,8 @@
+export interface City {
+	country: string;
+	lat: number;
+	local_names: Record<string, string>;
+	lon: number;
+	name: string;
+	state: string
+}
--- libs/weather-forecast/services/src/lib/weather-forecast-api.service.ts
+++ libs/weather-forecast/services/src/lib/weather-forecast-api.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { City } from './weather-forecast-api.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class WeatherForecastApiService {
-
+	private _url = 'http://api.openweathermap.org';
 	private _apiKey = '010721642521f31b0fbc8c3831d45951';
 
+	constructor(private readonly http: HttpClient) {}
+
+	getCity(cityName: string): Observable<City[]> {
+		return this.http.get<City[]>(`${this._url}/geo/1.0/direct?q=${cityName}&limit=1&appid=${this._apiKey}`);
+	}
 }
