271e80054857a7e5a2fe98e47c1df596d6545dba
Maxim Unguryan <makswww1@gmail.com>
2022-03-13 19:19:40 +0400

    made toggle button

--- apps/weather-forecast/src/app/components/toggle-button/toggle-button.component.ts
+++ apps/weather-forecast/src/app/components/toggle-button/toggle-button.component.ts
@@ -1,19 +1,48 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, forwardRef, Input, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const VALUE_ACCESSOR_PROVIDER: Provider = {
+	provide: NG_VALUE_ACCESSOR,
+	useExisting: forwardRef(() => ToggleButtonComponent),
+	multi: true,
+};
 
 @Component({
 	selector: 'app-toggle-button',
 	templateUrl: './toggle-button.component.html',
 	styleUrls: ['toggle-button.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [VALUE_ACCESSOR_PROVIDER],
 })
-export class ToggleButtonComponent implements OnInit {
-	@Input() mode: 'hourly' | 'daily' = 'hourly';
-	@Output() changeMode = new EventEmitter<'hourly' | 'daily'>();
+export class ToggleButtonComponent implements ControlValueAccessor {
+	@Input() leftLabel = '';
+	@Input() rightLabel = '';
 
-	checkedControl = new FormControl(this.mode === 'hourly');
+	val = true;
 
-	ngOnInit(): void {
-		this.checkedControl.valueChanges.subscribe(value => this.changeMode.emit(value ? 'daily' : 'hourly'));
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	onChange: (val: boolean) => void = () => {};
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	onTouch: () => void = () => {};
+
+	set value(val: boolean) {
+		this.onChange(val);
+		this.onTouch();
+	}
+
+	writeValue(value: boolean): void {
+		this.value = value;
+	}
+
+	registerOnChange(fn: (val: boolean) => void): void {
+		this.onChange = fn;
+	}
+
+	registerOnTouched(fn: () => void): void {
+		this.onTouch = fn;
+	}
+
+	click(value: boolean): void {
+		this.value = value;
 	}
 }
